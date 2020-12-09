be35191c0fbeebeb0b3cc2132ee5121e27b8f447
Florian Klämpfl <florian@freepascal.org>
2020-12-09 21:14:47 +0100

    * set main result value taking less instructions   * compile with -Os

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 FPC=$HOME/DevPriv/fpc-bin/m68k-amiga-020
 FPCBIN=$FPC/lib/fpc/3.3.1/ppcross68k
 
-FPCARGS="-Tamiga -O4 -Cp68000 -Avasm -XV -al "
+FPCARGS="-Tamiga -Os4 -Cp68000 -Avasm -XV -al "
 FPCARGS+="-FD$HOME/DevPriv/m68k-amiga-binutils "
 FPCARGS+="-Fu./ -Fu$FPC/lib/fpc/3.3.1/units/m68k-amiga/* "
 
